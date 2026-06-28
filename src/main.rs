2f267a1d2200e4cc1f6728baeb30c33cc6970053
Commensalism <folouindustries@protonmail.com>
2026-06-28 17:33:43 +0300

    Jettison file-based count modes
    Get rid of all file-based counting modes and move them into another
    branch.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-use std::{env::current_dir, ffi::OsString, fmt::Display, fs, io::Write, path::PathBuf, process::ExitCode, sync::{Arc, Mutex}, thread::spawn, time::Duration};
+use std::{env::current_dir, ffi::OsString, fmt::Display, fs, path::PathBuf, process::ExitCode, time::Duration};
 use colored::Colorize;
-use clap::{Parser, Subcommand, ValueEnum};
+use clap::{Parser, Subcommand};
 use indicatif::MultiProgress;
-use tar::Archive;
 
 mod operations;
 mod style;
@@ -41,10 +40,6 @@ enum Subcommands {
         #[arg(short, long, default_value_t = false)]
         verbose: bool,
 
-        /// Mode
-        #[arg(short, long, default_value_t = Mode::Direct)]
-        mode: Mode,
-
         /// No progress bar
         #[arg(short, long = "no-progress", default_value_t = false)]
         nopb: bool,
@@ -81,29 +76,6 @@ impl Display for Format {
     }
 }
 
-#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
-enum Mode {
-    Memory,
-    Processor,
-    Storage,
-    StorageKeep,
-    Sync,
-    Direct
-}
-
-impl Display for Mode {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Mode::Memory => write!(f, "memory"),
-            Mode::Processor => write!(f, "processor"),
-            Mode::Storage => write!(f, "storage"),
-            Mode::StorageKeep => write!(f, "storage-keep"),
-            Mode::Sync => write!(f, "sync"),
-            Mode::Direct => write!(f, "direct")
-        }
-    }
-}
-
 fn main() -> Result<ExitCode, Box<dyn std::error::Error>> {
     let cli = Cli::parse();
 
@@ -134,7 +106,7 @@ fn main() -> Result<ExitCode, Box<dyn std::error::Error>> {
             }
             Ok(if suc { ExitCode::SUCCESS } else { ExitCode::FAILURE })
         }
-        Some(Subcommands::Extract { file, dst, verbose, mode, nopb, sync_before, filters}) => {
+        Some(Subcommands::Extract { file, dst, verbose, nopb, sync_before, filters}) => {
             let cwd = current_dir()?;
             let dst = PathBuf::from(if let Some(p) = dst {
                 p
@@ -143,14 +115,12 @@ fn main() -> Result<ExitCode, Box<dyn std::error::Error>> {
                 cwd.as_os_str()
             });
             let name = String::from(PathBuf::from(file).file_name().unwrap().to_string_lossy());
-            let name2 = name.clone();
             let verbose = *verbose;
             let filters = filters.clone();
             let fm = operations::determine_format(file)?;
-            let mpb = Arc::new(MultiProgress::new());
+            let mpb = MultiProgress::new();
             let mainpb = mpb.add(style::themed_spinner().with_message("Preparing..."));
             mainpb.enable_steady_tick(Duration::from_millis(100));
-            let pb = Arc::new(Mutex::new(None));
 
             if *sync_before {
                 let f = fs::File::open(file)?;
@@ -158,110 +128,23 @@ fn main() -> Result<ExitCode, Box<dyn std::error::Error>> {
             }
             if !*nopb
             {
-                match *mode {
-                    Mode::Direct => {
-                        let f = fs::File::open(file)?;
-                        let pb = mpb.add(style::themed_progressbar_bytes(f.metadata()?.len()));
-                        let wr = pb.wrap_read(f);
-                        let mut arq = operations::prepare_archive_from_read(wr, fm)?;
-                        let fname = PathBuf::from(file).file_name().map(|f| f.to_owned());
-                        if let Some(name) = fname {
-                            mainpb.set_message(format!("Extracting {}...", name.to_string_lossy().yellow().bold()));
-                        }
-                        else {
-                            mainpb.set_message("Extracting...");
-                        }
-                        arq.unpack(dst)?;
-                    }
-                    Mode::Processor => {
-                        let arq = operations::prepare_archive(file, fm)?;
-                        let (ampb, apb) = (mpb.clone(), pb.clone());
-                        let _chandler = spawn(move || operations::count_archive_and_add_pb(arq, ampb, apb, name));
-                        let arq = operations::prepare_archive(file, fm)?;
-                        mainpb.set_message("Extracting...");
-                        let ehandler = spawn(move || operations::extract_archive_with_progress(arq, dst, mpb.clone(), name2, mainpb, pb.clone(), verbose, filters));
-                        ehandler.join().unwrap();
-                    }
-                    Mode::Memory => {
-                        let mut buf: Vec<u8> = Vec::new();
-                        operations::read_compressed_to_buf(file, &mut buf, fm)?;
-                        let buf2 = buf.clone();
-                        let (ampb, apb) = (mpb.clone(), pb.clone());
-                        let _chandler = spawn(move || operations::count_archive_and_add_pb_vec(buf, ampb, apb, name));
-                        mainpb.set_message("Extracting...");
-                        let ehandler = spawn(move || operations::extract_archive_with_progress_vec(buf2, dst, mpb.clone(), name2, mainpb, pb.clone(), verbose, filters));
-                        ehandler.join().unwrap();
-                    },
-                    Mode::Storage => {
-                        if let Format::Tar = fm {
-                            mpb.suspend(|| eprintln!("Storage mode is redundant with an uncompressed tar archive"));
-                            let (ampb, apb) = (mpb.clone(), pb.clone());
-                            let arq = operations::prepare_archive(file, fm)?;
-                            let _chandler = spawn(move || operations::count_archive_and_add_pb(arq, ampb, apb, name));
-                            let arq = operations::prepare_archive(file, fm)?;
-                            mainpb.set_message("Extracting...");
-                            let ehandler = spawn(move || operations::extract_archive_with_progress(arq, dst, mpb.clone(), name2, mainpb, pb.clone(), verbose, filters));
-                            ehandler.join().unwrap();
-                        }
-                        else {
-                            let fpath = format!(".dtar.{}.tar", file.to_string_lossy());
-                            let mut buf = fs::File::create(&fpath)?;
-                            operations::read_compressed_to_buf(file, &mut buf, fm)?;
-                            buf.flush()?;
-                            let buf = fs::File::open(&fpath)?;
-                            let buf2 = fs::File::open(&fpath)?;
-                            let (ampb, apb) = (mpb.clone(), pb.clone());
-                            let arq = Archive::new(buf);
-                            let _chandler = spawn(move || operations::count_archive_and_add_pb(arq, ampb, apb, name));
-                            let arq = Archive::new(buf2);
-                            mainpb.set_message("Extracting...");
-                            let ehandler = spawn(move || operations::extract_archive_with_progress(arq, dst, mpb.clone(), name2, mainpb, pb.clone(), verbose, filters));
-                            ehandler.join().unwrap();
-                            fs::remove_file(&fpath)?;
-                        }
-                    },
-                    Mode::StorageKeep => {
-                        if let Format::Tar = fm {
-                            mpb.suspend(|| eprintln!("Storage mode is redundant with an uncompressed tar archive"));
-                            let (ampb, apb) = (mpb.clone(), pb.clone());
-                            let arq = operations::prepare_archive(file, fm)?;
-                            let _chandler = spawn(move || operations::count_archive_and_add_pb(arq, ampb, apb, name));
-                            let arq = operations::prepare_archive(file, fm)?;
-                            mainpb.set_message("Extracting...");
-                            let ehandler = spawn(move || operations::extract_archive_with_progress(arq, dst, mpb.clone(), name2, mainpb, pb.clone(), verbose, filters));
-                            ehandler.join().unwrap();
-                        }
-                        else {
-                            let fpath = format!(".dtar.{}.tar", file.to_string_lossy());
-                            let mut buf = fs::File::create(&fpath)?;
-                            operations::read_compressed_to_buf(file, &mut buf, fm)?;
-                            buf.flush()?;
-                            let buf = fs::File::open(&fpath)?;
-                            let buf2 = fs::File::open(&fpath)?;
-                            let (ampb, apb) = (mpb.clone(), pb.clone());
-                            let arq = Archive::new(buf);
-                            let _chandler = spawn(move || operations::count_archive_and_add_pb(arq, ampb, apb, name));
-                            let arq = Archive::new(buf2);
-                            mainpb.set_message("Extracting...");
-                            let ehandler = spawn(move || operations::extract_archive_with_progress(arq, dst, mpb.clone(), name2, mainpb, pb.clone(), verbose, filters));
-                            ehandler.join().unwrap();
-                            fs::rename(&fpath, format!("{}.tar", file.to_string_lossy()))?;
-                        }
-                    },
-                    Mode::Sync => {
-                        let arq = operations::prepare_archive(file, fm)?;
-                        operations::count_archive_and_add_pb(arq, mpb.clone(), pb.clone(), name.clone());
-                        let arq = operations::prepare_archive(file, fm)?;
-                        operations::extract_archive_with_progress(arq, dst, mpb, name, mainpb, pb, verbose, filters);
-                    }
-                    #[allow(unreachable_patterns)]
-                    _ => todo!("Mode not implemented")
+                let f = fs::File::open(file)?;
+                let pb = mpb.add(style::themed_progressbar_bytes(f.metadata()?.len()));
+                let wr = pb.wrap_read(f);
+                let arq = operations::prepare_archive_from_read(wr, fm)?;
+                let fname = PathBuf::from(file).file_name().map(|f| f.to_owned());
+                if let Some(name) = fname {
+                    mainpb.set_message(format!("Extracting {}...", name.to_string_lossy().yellow().bold()));
                 }
+                else {
+                    mainpb.set_message("Extracting...");
+                }
+                operations::extract_archive_with_pb(arq, dst, &mpb, name, &mainpb, &pb, verbose, filters);
             }
             else {
                 let arq = operations::prepare_archive(file, fm)?;
                 mainpb.set_message("Extracting...");
-                operations::extract_archive_with_progress(arq, dst, mpb.clone(), name2, mainpb, pb.clone(), verbose, filters);
+                operations::extract_archive_no_pb(arq, dst, &mpb, &mainpb, verbose, filters);
             }
             Ok(ExitCode::SUCCESS)
         }
--- src/operations.rs
+++ src/operations.rs
@@ -1,4 +1,4 @@
-use std::{error::Error, ffi::{OsStr, OsString}, fs::{self, File}, io::{self, BufReader, Read, Write}, path::PathBuf, sync::{Arc, Mutex}};
+use std::{error::Error, ffi::{OsStr, OsString}, fs::{self, File}, io::{self, BufReader, Read}, path::PathBuf};
 use flate2::bufread::GzDecoder;
 use indicatif::{HumanCount, MultiProgress, ProgressBar};
 use xz2::bufread::XzDecoder;
@@ -37,35 +37,6 @@ impl std::fmt::Display for FormatError {
     }
 }
 
-pub fn read_compressed_to_buf(path: &OsStr, buf: &mut impl Write, format: Format) -> io::Result<()> {
-    match format {
-        Format::Gzip => {
-            let r = BufReader::new(File::open(path)?);
-            let mut dec = GzDecoder::new(r);
-            io::copy(&mut dec, buf)?;
-            Ok(())
-        }
-        Format::Xz => {
-            let r = BufReader::new(File::open(path)?);
-            let mut dec = XzDecoder::new(r);
-            io::copy(&mut dec, buf)?;
-            Ok(())
-        }
-        Format::Tar => {
-            let mut r: BufReader<File> = BufReader::new(File::open(path)?);
-            io::copy(&mut r, buf)?;
-            Ok(())
-        },
-        Format::Zstd => {
-            let r: BufReader<File> = BufReader::new(File::open(path)?);
-            let mut dec = zstd::Decoder::new(r)?;
-            io::copy(&mut dec, buf)?;
-            Ok(())
-        }
-        // _ => Err(Box::new(FormatError {}))
-    }
-}
-
 pub fn prepare_archive_from_read(reader: impl io::Read + Send + 'static, format: Format) -> Result<Archive<Box<dyn Read + Send>>, Box<dyn std::error::Error>> {
     match format {
         Format::Gzip => {
@@ -100,44 +71,9 @@ pub fn prepare_archive(path: &OsStr, format: Format) -> Result<Archive<Box<dyn R
     prepare_archive_from_read(f, format)
 }
 
-pub fn _list_archive(mut arq: Archive<impl Read>) -> Result<Vec<OsString>, Box<dyn std::error::Error>> {
-    let mut res: Vec<OsString> = Vec::new();
-    let entries = arq.entries()?;
-    for e in entries {
-        let e = e?;
-        let p = e.path()?;
-        res.push(p.as_os_str().to_owned());
-    }
-    Ok(res)
-}
-
-pub fn count_archive_and_add_pb_vec(buf: Vec<u8>, mpb: Arc<MultiProgress>, pb: Arc<Mutex<Option<ProgressBar>>>, name: String) {
-    let arq = Archive::new(buf.as_slice());
-    count_archive_and_add_pb(arq, mpb, pb, name);
-}
-
-pub  fn count_archive_and_add_pb(mut arq: Archive<impl Read>, mpb: Arc<MultiProgress>, pb: Arc<Mutex<Option<ProgressBar>>>, name: String) {
-    let count = arq.entries().unwrap().count();
-    let mut pb = pb.lock().unwrap();
-    *pb = Some(mpb.add(crate::style::themed_progressbar(count as u64).with_message(format!("Extracting {}...", name.cyan().bold()))));
-}
-
 #[allow(clippy::too_many_arguments)]
-pub fn extract_archive_with_progress_vec(buf: Vec<u8>, dst: PathBuf, mpb: Arc<MultiProgress>, name: String, mainpb: ProgressBar, pbr: Arc<Mutex<Option<ProgressBar>>>, verbose: bool, filters: Vec<OsString>) {
-    let arq = Archive::new(buf.as_slice());
-    extract_archive_with_progress(arq, dst, mpb, name, mainpb, pbr, verbose, filters);
-}
-
-#[allow(clippy::too_many_arguments)]
-pub fn extract_archive_with_progress(mut arq: Archive<impl Read>, dst: PathBuf, mpb: Arc<MultiProgress>, name: String, mainpb: ProgressBar, pbr: Arc<Mutex<Option<ProgressBar>>>, verbose: bool, filters: Vec<OsString>) {
-    let mut sofar: u64 = 0;
-    let mut skip: u64 = 0;
-    let mut pb_exists = false;
-
-    if let Some(ref pb) = *pbr.lock().unwrap() {
-        pb_exists = true;
-        pb.set_message(format!("Extracting {}...", name.cyan().bold()));
-    }
+pub fn extract_archive_with_pb(mut arq: Archive<impl Read>, dst: PathBuf, mpb: &MultiProgress, name: String, mainpb: &ProgressBar, pb: &ProgressBar, verbose: bool, filters: Vec<OsString>) {
+    pb.set_message(format!("Extracting {}...", name.cyan().bold()));
 
     if dst.symlink_metadata().is_err() {
         fs::create_dir_all(&dst).unwrap();
@@ -158,30 +94,63 @@ pub fn extract_archive_with_progress(mut arq: Archive<impl Read>, dst: PathBuf, 
                 }
             }
             if !pass {
-                skip += 1;
-                if let Some(ref pb) = *pbr.lock().unwrap() {
-                    pb_exists = true;
-                    pb.set_length(pb.length().unwrap() - skip);
-                    skip = 0;
-                }
                 continue;
             }
         }
         if let Some(name) = fname {
-            if !pb_exists {
-                mainpb.set_message(format!("{} Extracting {}...", format!("({})", HumanCount(sofar)).white(), name.to_string_lossy().yellow().bold()));
-            }
-            else {
-                mainpb.set_message(format!("Extracting {}...", name.to_string_lossy().yellow().bold()));
-            }
+            mainpb.set_message(format!("Extracting {}...", name.to_string_lossy().yellow().bold()));
         }
         else {
-            if !pb_exists {
-                mainpb.set_message(format!("{} Extracting...", format!("({})", HumanCount(sofar)).white()));
+            mainpb.set_message("Extracting...");
+        }
+        if verbose
+            && let Some(name) = fname {
+                mpb.println(format!("Extracting {}...", name.to_string_lossy().yellow().bold())).unwrap();
+        }
+        if file.header().entry_type() == tar::EntryType::Directory {
+            directories.push(file);
+        } else {
+            file.unpack_in(dst).unwrap();
+        }
+    }
+
+    directories.sort_by(|a, b| b.path_bytes().cmp(&a.path_bytes()));
+    for mut dir in directories {
+        dir.unpack_in(dst).unwrap();
+    }
+    pb.finish_with_message(format!("Extracted {}", name.cyan().bold()));
+    mainpb.finish_with_message("Done");
+}
+
+pub fn extract_archive_no_pb(mut arq: Archive<impl Read>, dst: PathBuf, mpb: &MultiProgress, mainpb: &ProgressBar, verbose: bool, filters: Vec<OsString>) {
+    let mut sofar: u64 = 0;
+    if dst.symlink_metadata().is_err() {
+        fs::create_dir_all(&dst).unwrap();
+    }
+
+    let dst = &dst.canonicalize().unwrap_or(dst.to_path_buf());
+
+    let mut directories = Vec::new();
+    for entry in arq.entries().unwrap() {
+        let mut file = entry.unwrap();
+        let fpath = file.path().unwrap();
+        let fname = fpath.file_name();
+        if !filters.is_empty() {
+            let mut pass = false;
+            for filter in &filters {
+                if fpath.starts_with(filter) {
+                    pass = true;
+                }
             }
-            else {
-                mainpb.set_message("Extracting...");
+            if !pass {
+                continue;
             }
+        }
+        if let Some(name) = fname {
+            mainpb.set_message(format!("{} Extracting {}...", format!("({})", HumanCount(sofar)).white(), name.to_string_lossy().yellow().bold()));
+        }
+        else {
+            mainpb.set_message(format!("{} Extracting...", format!("({})", HumanCount(sofar)).white()));
         }
         if verbose
             && let Some(name) = fname {
@@ -193,24 +162,11 @@ pub fn extract_archive_with_progress(mut arq: Archive<impl Read>, dst: PathBuf, 
             file.unpack_in(dst).unwrap();
         }
         sofar += 1;
-        if let Some(ref pb) = *pbr.lock().unwrap() {
-            pb_exists = true;
-            pb.set_position(sofar);
-        }
     }
 
     directories.sort_by(|a, b| b.path_bytes().cmp(&a.path_bytes()));
     for mut dir in directories {
         dir.unpack_in(dst).unwrap();
-        sofar += 1;
-        if let Some(ref pb) = *pbr.lock().unwrap() {
-            // pb_exists = true;
-            pb.set_position(sofar);
-        }
-    }
-    if let Some(ref pb) = *pbr.lock().unwrap() {
-        // pb_exists = true;
-        pb.finish_with_message(format!("Extracted {}", name.cyan().bold()));
     }
     mainpb.finish_with_message("Done");
 }
